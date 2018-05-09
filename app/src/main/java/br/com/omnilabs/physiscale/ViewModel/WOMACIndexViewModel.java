2026c518d907f05ece120de9f67a963a8db3e324
Roger <rogerdenis@gmail.com>
2018-05-09 17:00:45 -0300

    Creating VM Bindind I'm trying to bond a viewmodel between Layout and Activity, like a 2-way data binding

--- app/src/main/java/br/com/omnilabs/physiscale/ViewModel/WOMACIndexViewModel.java
+++ app/src/main/java/br/com/omnilabs/physiscale/ViewModel/WOMACIndexViewModel.java
@@ -1,4 +1,41 @@
 package br.com.omnilabs.physiscale.ViewModel;
 
-public class WOMACIndexViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.util.Log;
+
+import java.util.Observable;
+
+import br.com.omnilabs.physiscale.BR;
+
+public class WOMACIndexViewModel extends BaseObservable {
+    public int mSeekValue;
+    public int mSeekDisplay;
+
+    @Bindable
+    public int getSeekValue() {
+        Log.w("TESTE","Testando");
+        return mSeekValue;
+    }
+
+    public void setSeekValue(int progress) {
+        Log.w("TESTE","Testando");
+        mSeekValue = progress;
+        notifyPropertyChanged(BR.seekValue);
+        setSeekDisplay(progress);
+    }
+
+    @Bindable
+    public String getSeekDisplay() {
+        Log.w("TESTE","Testando");
+        return Integer.toString(mSeekDisplay);
+    }
+
+    public void setSeekDisplay(int progress) {
+        Log.w("TESTE","Testando");
+        mSeekDisplay = progress;
+        notifyPropertyChanged(BR.seekDisplay);
+    }
+
+
 }

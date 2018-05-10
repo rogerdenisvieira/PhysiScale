94873bb38d0f058aaceb2809848e28ebc234a919
Roger <rogerdenis@gmail.com>
2018-05-10 16:11:19 -0300

    Created a fragment Changing the architecture to use Android fragments in a listview

--- app/src/main/java/br/com/omnilabs/physiscale/Activity/WOMACIndexActivity.java
+++ app/src/main/java/br/com/omnilabs/physiscale/Activity/WOMACIndexActivity.java
@@ -10,6 +10,6 @@ public class WOMACIndexActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_womacscale);
+        setContentView(R.layout.activity_womacindex);
     }
 }
---
+++ app/src/main/java/br/com/omnilabs/physiscale/Activity/WOMACIndexFragment.java
@@ -0,0 +1,25 @@
+package br.com.omnilabs.physiscale.Activity;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.omnilabs.physiscale.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WOMACIndexFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WOMACIndexFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WOMACIndexFragment extends ListFragment {
+
+}
